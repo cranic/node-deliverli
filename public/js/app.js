379aca1cacca1d0211062edc2cfa0bbfc18ef161
Alan Hoffmeister <alanhoffmeister@gmail.com>
2014-06-03 10:44:58 -0300

    angularjs testing

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['ngRoute', 'ui.bootstrap']);
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
-    $http.get('http://api.jsdelivr.com/v1/jsdelivr/libraries').then(function(res){
+    $http.get('https://api.jsdelivr.com/v1/jsdelivr/libraries').then(function(res){
         console.log(res.data.length);
     });
 }]);
